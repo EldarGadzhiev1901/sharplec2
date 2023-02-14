fc361e5151428c68c8a308465ef63098266c8c06
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 19:04:33 +0300

    exp2

---
+++ exp2/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 11, 21, 31, 41, 1303, 61, 2, 3, 0};
+
+int n = array.Length;
+int find = 41;
+
+int index = 0;
+
+while (index < n)
+
+{
+   if(array[index] == find)
+   {
+    Console.WriteLine(index);
+    break;
+   }
+
+index++;
+
+
+}
